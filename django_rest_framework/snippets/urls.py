89490fb98c9233727195c7a50a5d9e0b75be9052
Aaditya Agrawal <aaditya.agrawal@cuelogic.com>
2017-03-02 18:27:03 +0530

    Request/resposne objects and hanlde mechanism

--- django_rest_framework/snippets/urls.py
+++ django_rest_framework/snippets/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import url, include
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from rest_framework import routers
 from snippets.serializers import SnippetsViewSet
 router = routers.DefaultRouter()
 router.register(r'ui_snippets', SnippetsViewSet)
 
 urlpatterns = [
-	url(r'^', include(router.urls)),
+	# url(r'^', include(router.urls)),
 	url(r'^snippets/$', views.snippet_list, name="snippets_list"),
 	url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail, name="snippets_details"),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
